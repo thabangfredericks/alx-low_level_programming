707a6a7e9bd8b09a7a5b66fe27d0917b2bd5016b
thabangfredericks <thabangfredricks@gmail.com>
2023-02-23 23:55:35 -0800

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * nul - multiples tow integers
+ * @x: The first number to multiply
+ * @y: The second number to multiply with first number
+ * Return: The result of multiplication between x param and y param
+ */
+
+int nul(int x, int y)
+{
+	return (x * y);
+}
