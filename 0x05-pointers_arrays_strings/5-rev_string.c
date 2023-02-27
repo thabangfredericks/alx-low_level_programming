94c5c13b37acdab9eedba3414da450ff60454af7
thabangfredericks <thabangfredricks@gmail.com>
2023-02-27 06:02:50 -0800

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,18 +7,17 @@
  */
 
 void rev_string(char *s)
-
 {
-	int len = 0, index = 0;
-	char tmp;
+int len = 0, index = 0;
+char tmp;
 
-	while (s[index++])
-	len++;
+while (s[index++])
+len++;
 
-	for (indec = len - 1; index >= len / 2; index--)
-	{
-	tmp = s[index];
-	s[index] = s[len - index - 1];
-	s[len - index - 1} = tmp
-	}
+for (index = len - 1; index >= len / 2; index--)
+{
+tmp = s[index];
+s[index] = s[len - index - 1];
+s[len - index - 1] = tmp;
+}
 }
