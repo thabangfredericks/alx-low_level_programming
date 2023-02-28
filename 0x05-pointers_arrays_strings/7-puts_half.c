e9a556ae4f0e5c86c81c9631792fe9065329d204
thabangfredericks <thabangfredricks@gmail.com>
2023-02-27 16:54:33 -0800

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -16,16 +16,22 @@ while (str[j] != '\0')
 {
 j++;
 }
+
 if (j % 2 == 1)
 {
 k = (j - 1) / 2;
 k += 1;
+}
+
+else
 {
 k = j / 2;
 }
+
 for (; k < j; k++)
 {
 _putchar(str[k]);
 }
 _putchar('\n');
+
 }
